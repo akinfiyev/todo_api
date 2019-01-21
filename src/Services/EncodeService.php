70a2e6739a93dd621859a80f22139fc3c443d4c6
sliceice <mr.sliceice@gmail.com>
2019-01-21 23:00:36 +0200

    Services and listeners add

---
+++ src/Services/EncodeService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class EncodeService
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function encodeUserPassword(?String $plainPassword, User $user)
+    {
+        if (!empty($plainPassword)) {
+            return $this->encoder->encodePassword($user, $plainPassword);
+        }
+
+        return $plainPassword;
+    }
+}
