fa970340fa7992405e2998442e2badaf89ce76db
sliceice <mr.sliceice@gmail.com>
2019-01-27 14:14:02 +0200

    JsonHttpException

---
+++ src/Listener/JsonExceptionListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listener;
+
+use App\Exception\JsonHttpException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class JsonExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        if ($exception instanceof JsonHttpException) {
+            $errorData = [
+                'error' => [
+                    'code' => $exception->getStatusCode(),
+                    'message' => $exception->getMessage(),
+                ]
+            ];
+            if (($data = $exception->getData())) {
+                $errorData['error']['fields'] = $data;
+            }
+            $response = new JsonResponse($errorData);
+            $event->setResponse($response);
+        }
+    }
+}
