a882e3d53be2b8fe91df8f5d4bc133bb64de9d71
sliceice <mr.sliceice@gmail.com>
2019-01-27 22:19:00 +0200

    Composer install: guzzle; UserController unit test

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
 ];
--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -2,12 +2,25 @@
 
 namespace App\Tests;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function registrationTest()
+    public function testRegistration()
     {
+        $data = json_encode([
+            'email' => 'email' . time() . "@gmail.com",
+            'plainPassword' => "12345"
+        ]);
 
+        $client = new Client();
+        $response = $client->post('http://127.0.0.10:8080/api/users', [
+            'body' => $data
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $responseBody = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('apiToken', $responseBody);
     }
 }
