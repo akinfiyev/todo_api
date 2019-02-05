a284f5c756d261d154d0aebad64c952e1bbc815c
sliceice <mr.sliceice@gmail.com>
2019-02-05 22:03:53 +0200

    LabelNormalizer

---
+++ src/Normalizer/LabelNormalizer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: q
+ * Date: 27.01.19
+ * Time: 22:20
+ */
+
+namespace App\Normalizer;
+
+use App\Entity\Label;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class LabelNormalizer implements NormalizerInterface
+{
+    /**
+     * @param Label $label
+     * @param null $format
+     * @param array $context
+     * @return array|bool|float|int|string
+     */
+    public function normalize($label, $format = null, array $context = [])
+    {
+        return [
+            "title" => $label->getTitle()
+        ];
+    }
+
+    public function supportsNormalization($label, $format = null)
+    {
+        return $label instanceof Label;
+    }
+
+}
