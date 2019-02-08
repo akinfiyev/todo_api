5adb524030840c337105dc2cffc33728a17d6581
sliceice <mr.sliceice@gmail.com>
2019-02-08 14:03:59 +0200

    LabelService

---
+++ src/Services/LabelService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\ItemList;
+use App\Entity\Label;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LabelService
+{
+    /**
+     * @var ObjectManager
+     */
+    private $om;
+
+    /**
+     * LabelService constructor.
+     * @param ObjectManager $om
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    /**
+     * @param ArrayCollection $labels
+     * @param ItemList $itemList
+     * @return ArrayCollection
+     */
+    public function getSyncLabelsArray(ArrayCollection $labels, ItemList $itemList): ArrayCollection
+    {
+        $labels =  array_unique($labels->toArray());
+
+        /** @var Label $label */
+        foreach ($labels as $label) {
+            $savedLabel = $this->om->getRepository(Label::class)->findOneByTitle($label->getTitle());
+            if (isset($savedLabel)) {
+                if (($key = array_search($label, $labels)) !== false)
+                    unset($labels[$key]);
+
+                $labels[] = $savedLabel;
+                $savedLabel->addItemList($itemList);
+            }
+        }
+
+        return new ArrayCollection($labels);
+    }
+}
