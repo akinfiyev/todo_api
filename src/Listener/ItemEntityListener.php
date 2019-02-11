5aea96537f6dd8d08a1658e9388175b099541ae1
sliceice <mr.sliceice@gmail.com>
2019-02-11 19:10:22 +0200

    Erase unused injection

--- src/Listener/ItemEntityListener.php
+++ src/Listener/ItemEntityListener.php
@@ -9,26 +9,10 @@
 namespace App\Listener;
 
 use App\Entity\Item;
-use App\Services\EncodeService;
 use Doctrine\ORM\Mapping\PreFlush;
 
 class ItemEntityListener
 {
-    /**
-     * @var EncodeService
-     */
-    private $encodeService;
-
-    /**
-     * UserListener constructor
-     *
-     * @param $encodeService
-     */
-    public function __construct(EncodeService $encodeService)
-    {
-        $this->encodeService = $encodeService;
-    }
-
     /** @PreFlush */
     public function preFlushHandler(Item $item)
     {
